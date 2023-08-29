4821df450ce118e26a085b13ad914d2951930718
luispark6 <123977565+luispark6@users.noreply.github.com>
2023-08-28 20:45:14 -0400

    finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Hyperspace by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
@@ -108,11 +102,8 @@
 
 							}
 						});
-
 				});
-
 		}
-
 	// Scrolly.
 		$('.scrolly').scrolly({
 			speed: 1000,
